d237ee1b0da84538246db640d72693690e8d8215
abnekhlifa <ala.ben-khlifa@talan.com>
2020-02-20 15:15:15 +0100

    Test Git WorkFlow #1

--- src/main/java/com/example/builddemo/BuilddemoApplication.java
+++ src/main/java/com/example/builddemo/BuilddemoApplication.java
@@ -1,14 +1,16 @@
 package com.example.builddemo;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class BuilddemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BuilddemoApplication.class, args);
-        
+        log.error("Hello World !");
     }
-
 }
