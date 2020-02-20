00722a1762fd8029481acbe25c5dbbd973236f58
abnekhlifa <ala.ben-khlifa@talan.com>
2020-02-20 12:25:41 +0100

    Initial commit

---
+++ src/test/java/com/example/builddemo/BuilddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.builddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuilddemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
