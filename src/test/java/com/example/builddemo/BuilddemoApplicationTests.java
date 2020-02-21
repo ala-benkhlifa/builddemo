279e0cacdce90e6ec1eff276ae8bf38731353270
abnekhlifa <ala.ben-khlifa@talan.com>
2020-02-21 16:06:21 +0100

    Sonar #2

--- src/test/java/com/example/builddemo/BuilddemoApplicationTests.java
+++ src/test/java/com/example/builddemo/BuilddemoApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.builddemo;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,11 +8,5 @@ class BuilddemoApplicationTests {
 
     @Test
     void contextLoads() {
-    }
-
-    @Test
-    void testSumFunction() {
-        DummyClass dummyClass = new DummyClass();
-        Assertions.assertEquals(3, dummyClass.somme(1,2));
     }
 }
