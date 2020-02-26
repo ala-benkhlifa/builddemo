d4ebc46c57ae3ec3a35827687853aae1baa8f605
abnekhlifa <ala.ben-khlifa@talan.com>
2020-02-26 16:50:01 +0100

    CORS

--- src/main/java/com/example/builddemo/StudentRestController.java
+++ src/main/java/com/example/builddemo/StudentRestController.java
@@ -1,10 +1,12 @@
 package com.example.builddemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class StudentRestController {
 
     @Autowired
